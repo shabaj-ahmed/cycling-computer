6c3204ecb12ea7fa0c4b5f5390afce338bd9a254
msahmed1 <shabaj_ahmed@outlook.com>
2025-07-07 02:22:29 +0100

    Refactor main.cpp to remove unused Arduino.h and streamline setup process

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,16 @@
-#include <Arduino.h>
+#include "M5Unified.h"
 #include "HeartRateSensor.h"
+#include "DisplayManager.h"
 
 HeartRateSensor heartRate;
-
-void heartRateTask(void* pvParameters) {
-    while (true) {
-        heartRate.scanAndConnect();
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
+DisplayManager display;
 
 void setup() {
     Serial.begin(115200);
+
     heartRate.begin();
-    delay(1000);
-    Serial.println("Setup started!");
-    xTaskCreatePinnedToCore(heartRateTask, "HeartRate", 4096, NULL, 1, NULL, 1);
+    display.begin();
 }
+
 void loop() {
 }
