b291414aada77ee24b6761ef5aa77b0618396d55
msahmed1 <shabaj_ahmed@outlook.com>
2025-07-10 01:35:00 +0100

    Implement dispatcher task for event handling and update display manager integration

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,28 @@ DisplayManager displayManager;
 ButtonManager buttonManager;
 
 QueueHandle_t eventQueue;
+QueueHandle_t displayQueue;
+
+void dispatcherTask(void* pvParameters) {
+    SystemEvent evt;
+    while (true) {
+        if (xQueueReceive(eventQueue, &evt, portMAX_DELAY) == pdTRUE) {
+
+            if (evt.target == EventTarget::Display || evt.target == EventTarget::All)
+                {xQueueSend(displayQueue, &evt, 0);}
+
+        }
+    }
+}
 
 void setup() {
     Serial.begin(115200);
+    delay(3000); // Wait for Serial to initialize
 
-    QueueHandle_t eventQueue;
+    M5.begin();
+    eventQueue   = xQueueCreate(20, sizeof(SystemEvent));
+    displayQueue = xQueueCreate(10, sizeof(SystemEvent));
 
-    eventQueue = xQueueCreate(10, sizeof(SystemEvent));
     if (!eventQueue) {
         Serial.println("Failed to create event queue!");
         while (true);
@@ -23,8 +38,19 @@ void setup() {
 
     heartRate.begin(eventQueue);
     buttonManager.begin(eventQueue);
-    displayManager.begin(eventQueue);
+    displayManager.begin(displayQueue);
+
+    xTaskCreatePinnedToCore(
+        dispatcherTask,
+        "Dispatcher",
+        2048,
+        nullptr,
+        2,
+        nullptr,
+        1
+    );
 }
 
 void loop() {
+
 }
